5fcb5a86487efe6506929644acbedfdc7162eef6
SauloOliveira06 <oliveira.saulo06@gmail.com>
2021-08-23 20:22:52 -0400

    rental list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { RentalCard } from './components/rental/RentalCard';
 import { Header } from './shared/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='container'>
+        <section id='rentalListing'>
+          <h1 className='page-title'>Your Home All Around the World</h1>
+          <div className='row'>
+            <RentalCard />
+            <RentalCard />
+            <RentalCard />
+          </div>
+        </section>
+      </div>
+
     </div>
   );
 }
